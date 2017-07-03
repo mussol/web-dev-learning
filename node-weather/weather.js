781285627a3992eda76baf3a53fd889067b5569e
mussol <gemma.valls@gmail.com>
2017-07-03 22:21:49 +0200

    add style

--- node-weather/weather.js
+++ node-weather/weather.js
@@ -15,10 +15,10 @@ function getWeather (location, callback) {
 			callback({
 				desc: data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1) || '',
 				icon_url: data.weather[0].icon ? `http://openweathermap.org/img/w/${data.weather[0].icon}.png` : '',
-				loc: `${data.name}, ${data.sys.country}` || 'your location',
+				loc: data.name || 'your location',
 				temp: data.main.temp.toFixed(1),
-				temp_min: data.main.temp_min.toFixed(1),
-				temp_max: data.main.temp_max.toFixed(1),
+				temp_min: data.main.temp_min,
+				temp_max: data.main.temp_max,
 				humidity: Math.round(data.main.humidity),
 				wind: Number(data.wind.speed).toFixed(1),
 				units: units
