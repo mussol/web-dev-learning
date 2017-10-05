deb6e42d5bf44df5e8a11f28680ed22231a7a31d
mussol <gemma.valls@gmail.com>
2017-10-05 18:01:37 +0200

    separate components from rendering

---
+++ basic-MERN-app/client/components/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//define and export a simple component
+export default class App extends React.Component {
+    render() {
+        return <h1>Hello!</h1>
+    }
+}
---
+++ basic-MERN-app/client/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from 'components/app';
+
+//attach the imported App component to the div element #app in index.html
+ReactDOM.render(<App />, document.getElementById('app'));
