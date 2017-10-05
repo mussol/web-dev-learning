b60f19c67afa4e9d4b0855c6d668bfdbbb6b35d8
mussol <gemma.valls@gmail.com>
2017-10-05 18:08:57 +0200

    fix path to App component

--- basic-MERN-app/client/index.js
+++ basic-MERN-app/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'components/app';
+import App from './components/app';
 
 //attach the imported App component to the div element #app in index.html
 ReactDOM.render(<App />, document.getElementById('app'));
